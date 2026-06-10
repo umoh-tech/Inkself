0943ee2e56c6c3c2aed776994807692ac1b2473c
umoh-tech <umohdaniel62@gmail.com>
2026-06-10 17:41:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def generate():
 
     def stream_response():
         model = genai.GenerativeModel(
-            model_name="gemini-1.5-flash",
+            model_name="gemini-2.0-flash",
             system_instruction=system,
         )
         response = model.generate_content(prompt, stream=True)
